02ed205f5049281da20d49e1847d276c1aac4a87
Hamzehaboamer12 <hamzehaboamee@gmail.com>
2022-03-15 21:19:02 +0200

    add stack

---
+++ app/src/main/java/stackandqueue/Node.java
@@ -0,0 +1,16 @@
+package stackandqueue;
+
+public class Node<T> {
+
+    Node<T> next;
+    T value;
+
+    public Node(T value){
+        this.value = value;
+    }
+
+
+    public Node() {
+
+    }
+}

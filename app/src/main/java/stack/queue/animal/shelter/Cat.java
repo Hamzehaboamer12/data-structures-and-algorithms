7785bccd6f3cae3e1824188254cb8d365b933d13
Hamzehaboamer12 <hamzehaboamee@gmail.com>
2022-03-21 15:59:07 +0200

    add new

---
+++ app/src/main/java/stack/queue/animal/shelter/Cat.java
@@ -0,0 +1,14 @@
+package stack.queue.animal.shelter;
+
+public class Cat implements Animal{
+    private final String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName(){
+        return this.name;
+    }
+}
